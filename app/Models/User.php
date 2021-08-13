5d2c701c3679aec39fc84c4513a1da7092fef7a3
longnv-99 <longnv.089@gmail.com>
2021-08-13 16:33:03 +0700

    init db and model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -17,9 +16,12 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
+        'department_id',
         'name',
         'email',
         'password',
+        'address',
+        'position',
     ];
 
     /**
@@ -32,12 +34,8 @@ class User extends Authenticatable
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public function department()
+    {
+        return $this->belongsTo(Department::class);
+    }
 }
