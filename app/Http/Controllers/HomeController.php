4b7e12ebf2f784651dc4d008c644e23c784bd7a4
longnv-99 <longnv.089@gmail.com>
2021-08-17 08:36:25 +0700

    localization

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function home()
+    {
+        return view('home');
+    }
+
+    public function changeLanguage(Request $request, $locale)
+    {
+        if ($locale && in_array($locale, config('app.languages'))) {
+            $request->session()->put('language', $locale);
+        }
+
+        return redirect()->back();
+    }
+}
